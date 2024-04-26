17625db38830346c8ef91537261d52d12e19cb18
GhenaMasri <ghenama77@gmail.com>
2024-04-26 13:50:52 +0300

    Adding frontend validation

--- lib/signin.dart
+++ lib/signin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/signup.dart';
 
 class Signin extends StatefulWidget {
   const Signin({Key? key});
@@ -9,6 +10,11 @@ class Signin extends StatefulWidget {
 
 class _Signin extends State<Signin> {
   bool _passwordVisible = false;
+  GlobalKey<FormState> formState = GlobalKey();
+  String? email;
+  String? password;
+  String errorMessage = '';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -58,125 +64,182 @@ class _Signin extends State<Signin> {
                             topLeft: Radius.circular(60),
                             topRight: Radius.circular(60))),
                     child: Form(
+                        key: formState,
                         child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                        children: [
-                          const SizedBox(
-                            height: 60,
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(20),
-                            decoration: const BoxDecoration(
-                                color: Colors.white,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)),
-                                boxShadow: [
-                                  BoxShadow(
-                                      color:
-                                          Color.fromRGBO(240, 144, 104, 0.988),
-                                      blurRadius: 20,
-                                      offset: Offset(0, 10)),
-                                ]),
-                            child: Column(
-                              children: [
-                                Container(
-                                  padding: const EdgeInsets.all(10),
-                                  decoration: const BoxDecoration(
-                                      border: Border(
-                                          bottom: BorderSide(
-                                              color: Color.fromARGB(
-                                                  255, 238, 238, 238)))),
-                                  child: TextFormField(
-                                    decoration: const InputDecoration(
-                                        hintText: "Email",
-                                        hintStyle:
-                                            TextStyle(color: Colors.grey),
-                                        border: InputBorder.none),
-                                  ),
-                                ),
-                                Container(
-                                  padding: const EdgeInsets.all(10),
-                                  decoration: const BoxDecoration(
-                                      border: Border(
-                                          bottom: BorderSide(
-                                              color: Color.fromARGB(
-                                                  255, 238, 238, 238)))),
-                                  child: TextFormField(
-                                    decoration: InputDecoration(
-                                      hintText: "Password",
-                                      hintStyle: TextStyle(color: Colors.grey),
-                                      border: InputBorder.none,
-                                      suffixIcon: InkWell(
-                                        onTap: () {
-                                          setState(() {
-                                            _passwordVisible =
-                                                !_passwordVisible;
-                                          });
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            children: [
+                              const SizedBox(
+                                height: 60,
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(20),
+                                decoration: const BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10)),
+                                    boxShadow: [
+                                      BoxShadow(
+                                          color: Color.fromRGBO(
+                                              240, 144, 104, 0.988),
+                                          blurRadius: 20,
+                                          offset: Offset(0, 10)),
+                                    ]),
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      padding: const EdgeInsets.all(10),
+                                      decoration: const BoxDecoration(
+                                          border: Border(
+                                              bottom: BorderSide(
+                                                  color: Color.fromARGB(
+                                                      255, 238, 238, 238)))),
+                                      child: TextFormField(
+                                        onSaved: (newValue) {
+                                          email = newValue;
                                         },
-                                        child: Icon(
-                                          _passwordVisible
-                                              ? Icons.visibility
-                                              : Icons.visibility_off,
-                                          color: Colors.grey,
+                                        validator: (value) {
+                                          if (value!.isEmpty) {
+                                            return "Please enter the email";
+                                          }
+                                        },
+                                        decoration: const InputDecoration(
+                                            hintText: "Email",
+                                            hintStyle:
+                                                TextStyle(color: Colors.grey),
+                                            errorStyle: TextStyle(
+                                                color: Color.fromARGB(
+                                                    255, 230, 81, 0)),
+                                            border: InputBorder.none),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: const EdgeInsets.all(10),
+                                      decoration: const BoxDecoration(
+                                          border: Border(
+                                              bottom: BorderSide(
+                                                  color: Color.fromARGB(
+                                                      255, 238, 238, 238)))),
+                                      child: TextFormField(
+                                        obscureText: (!_passwordVisible),
+                                        onSaved: (newValue) {
+                                          password = newValue;
+                                        },
+                                        validator: (value) {
+                                          if (value!.isEmpty) {
+                                            return "Please enter your password";
+                                          }
+                                        },
+                                        decoration: InputDecoration(
+                                          hintText: "Password",
+                                          hintStyle:
+                                              TextStyle(color: Colors.grey),
+                                          errorStyle: TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 230, 81, 0)),
+                                          border: InputBorder.none,
+                                          suffixIcon: InkWell(
+                                            onTap: () {
+                                              setState(() {
+                                                _passwordVisible =
+                                                    !_passwordVisible;
+                                              });
+                                            },
+                                            child: Icon(
+                                              _passwordVisible
+                                                  ? Icons.visibility
+                                                  : Icons.visibility_off,
+                                              color: Colors.grey,
+                                            ),
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
+                                  ],
                                 ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 30,
-                          ),
-                          GestureDetector(
-                            onTap: () {},
-                            child: const Text(
-                              "Forgot Password?",
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Text("Don't have an account? "),
+                              ),
+                              const SizedBox(
+                                height: 30,
+                              ),
                               GestureDetector(
-                                onTap: () {},
-                                child: const Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 10),
-                                  child: Text(
-                                    "Sign up",
-                                    style: TextStyle(
-                                        color: Color.fromARGB(255, 230, 81, 0),
-                                        decoration: TextDecoration.underline),
-                                  ),
+                                onTap: () {
+                                  //Navigate to reset password page
+                                },
+                                child: const Text(
+                                  "Forgot Password?",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text("Don't have an account? "),
+                                  GestureDetector(
+                                    onTap: () {
+                                      Navigator.of(context).pushReplacement(
+                                          MaterialPageRoute(
+                                              builder: (context) => Signup()));
+                                    },
+                                    child: const Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 10),
+                                      child: Text(
+                                        "Sign up",
+                                        style: TextStyle(
+                                            color:
+                                                Color.fromARGB(255, 230, 81, 0),
+                                            decoration:
+                                                TextDecoration.underline),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              MaterialButton(
+                                onPressed: () {
+                                  if (formState.currentState!.validate()) {
+                                    formState.currentState!.save();
+                                    //check the credintials in db if correct navigate to home page
+                                    /*if( valid credintials ) {
+                                        setState(() {
+                                                errorMessage = "";
+                                              });
+                                      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+                                    }*/
+                                  } else {
+                                    setState(() {
+                                      errorMessage =
+                                          'Your email or password is incorrect';
+                                    });
+                                  }
+                                },
+                                color: const Color.fromARGB(255, 230, 81, 0),
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50)),
+                                minWidth: 200,
+                                height: 50,
+                                child: const Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 15),
+                                ),
+                              ),
+                              Text(
+                                errorMessage,
+                                style: TextStyle(
+                                  color: const Color.fromARGB(255, 230, 81, 0),
+                                  fontSize: 12,
                                 ),
                               )
                             ],
                           ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          MaterialButton(
-                            onPressed: () {},
-                            color: const Color.fromARGB(255, 230, 81, 0),
-                            elevation: 0,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50)),
-                            minWidth: 200,
-                            height: 50,
-                            child: const Text(
-                              "Login",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 15),
-                            ),
-                          )
-                        ],
-                      ),
-                    ))))
+                        ))))
           ],
         ),
       ),
