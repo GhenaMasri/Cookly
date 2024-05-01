3bacc3abd4e300bbfb91efb8895b4d4d95d89094
WasanAl-Sayed <wasanjehad75@gmail.com>
2024-05-01 23:24:58 +0300

    signing/backend implement signin

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ app.use(bodyParser.json());
 const signupRoute = require('./signup');
 app.use('/signup', signupRoute);
 
+const signinRoute = require('./signin');
+app.use('/signin', signinRoute);
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
