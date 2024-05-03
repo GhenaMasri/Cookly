4b708e3728ba9c54d8fd5c80ed4c882517ad14ff
GhenaMasri <ghenama77@gmail.com>
2024-05-03 16:12:11 +0300

    Editing home page design

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -4,7 +4,7 @@ import 'package:untitled/common_widget/dropdown.dart';
 import 'package:untitled/common_widget/round_textfield.dart';
 
 class HomeView extends StatefulWidget {
-  const HomeView({super.key});
+  const HomeView({Key? key}) : super(key: key);
 
   @override
   State<HomeView> createState() => _HomeViewState();
@@ -15,6 +15,21 @@ class _HomeViewState extends State<HomeView> {
   TextEditingController txtSearch = TextEditingController();
 
   List menuArr = [
+    {
+      "name": "Food",
+      "image": "assets/img/menu_1.png",
+      "items_count": "120",
+    },
+    {
+      "name": "Food",
+      "image": "assets/img/menu_1.png",
+      "items_count": "120",
+    },
+    {
+      "name": "Food",
+      "image": "assets/img/menu_1.png",
+      "items_count": "120",
+    },
     {
       "name": "Food",
       "image": "assets/img/menu_1.png",
@@ -41,26 +56,27 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     var media = MediaQuery.of(context).size;
     return Scaffold(
-        body: Stack(alignment: Alignment.centerLeft, children: [
-      Container(
-        margin: const EdgeInsets.only(top: 270),
-        width: media.width * 0.27,
-        height: media.height * 0.6,
-        decoration: BoxDecoration(
-          color: TColor.primary,
-          borderRadius: const BorderRadius.only(
-              topRight: Radius.circular(35), bottomRight: Radius.circular(35)),
-        ),
-      ),
-      SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          child: Column(
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            top: 250,
+            child: Container(
+              width: media.width * 0.27,
+              height: media.height,
+              decoration: BoxDecoration(
+                color: TColor.primary,
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(35),
+                  bottomRight: Radius.circular(35),
+                ),
+              ),
+            ),
+          ),
+          Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(
-                height: 46,
-              ),
+              const SizedBox(height: 46),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: Row(
@@ -77,9 +93,9 @@ class _HomeViewState extends State<HomeView> {
                     IconButton(
                       onPressed: () {
                         /* Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const Notifications()));*/
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Notifications()));*/
                       },
                       icon: Image.asset(
                         "assets/img/notification.png",
@@ -90,41 +106,32 @@ class _HomeViewState extends State<HomeView> {
                   ],
                 ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
+              const SizedBox(height: 10),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        "Delivering to",
-                        style: TextStyle(
-                          color: TColor.secondaryText,
-                          fontSize: 12,
-                        ),
+                    Text(
+                      "Delivering to",
+                      style: TextStyle(
+                        color: TColor.secondaryText,
+                        fontSize: 12,
                       ),
                     ),
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: MyDropdownMenu(
-                        value: selectedLocation,
-                        onChanged: (newValue) {
-                          setState(() {
-                            selectedLocation = newValue;
-                          });
-                        },
-                      ),
+                    const SizedBox(height: 5),
+                    MyDropdownMenu(
+                      value: selectedLocation,
+                      onChanged: (newValue) {
+                        setState(() {
+                          selectedLocation = newValue;
+                        });
+                      },
                     ),
                   ],
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
+              const SizedBox(height: 20),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: RoundTextfield(
@@ -137,90 +144,89 @@ class _HomeViewState extends State<HomeView> {
                       "assets/img/search.png",
                       width: 20,
                       height: 20,
+                      color: TColor.primary,
                     ),
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              ListView.builder(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-                physics: null,
-                shrinkWrap: true,
-                itemCount: menuArr.length,
-                itemBuilder: ((context, index) {
-                  var mObj = menuArr[index] as Map? ?? {};
-                  return GestureDetector(
-                    onTap: () {
-                      /*Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => MenuItemsView(
-                            mObj: mObj,
-                          ),
-                        ),
-                      );*/
-                    },
-                    child: Stack(
-                      alignment: Alignment.centerRight,
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.only(
-                              top: 8, bottom: 8, right: 20),
-                          width: media.width - 100,
-                          height: 90,
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(25),
-                              bottomLeft: Radius.circular(25),
-                              topRight: Radius.circular(10),
-                              bottomRight: Radius.circular(10),
-                            ),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black12,
-                                blurRadius: 7,
-                                offset: Offset(0, 4),
+              const SizedBox(height: 10),
+              Expanded(
+                child: ListView.builder(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                  itemCount: menuArr.length,
+                  itemBuilder: ((context, index) {
+                    var mObj = menuArr[index] as Map? ?? {};
+                    return GestureDetector(
+                      onTap: () {
+                        /*Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MenuItemsView(
+                                mObj: mObj,
                               ),
-                            ],
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                            ),
+                          );*/
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: Stack(
+                          alignment: Alignment.centerRight,
                           children: [
-                            Image.asset(
-                              mObj["image"].toString(),
-                              width: 80,
-                              height: 80,
-                              fit: BoxFit.contain,
-                            ),
-                            const SizedBox(
-                              width: 15,
-                            ),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                            Container(
+                              width: double.infinity,
+                              height: 90,
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(25),
+                                  bottomLeft: Radius.circular(25),
+                                  topRight: Radius.circular(25),
+                                  bottomRight: Radius.circular(25),
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black12,
+                                    blurRadius: 7,
+                                    offset: Offset(0, 4),
+                                  ),
+                                ],
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.center,
                                 children: [
-                                  Text(
-                                    mObj["name"].toString(),
-                                    style: TextStyle(
-                                      color: TColor.primaryText,
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.w700,
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 4,
-                                  ),
-                                  Text(
-                                    "${mObj["items_count"].toString()} items",
-                                    style: TextStyle(
-                                      color: TColor.secondaryText,
-                                      fontSize: 11,
+                                  ClipOval(
+                                      child: Image.asset(
+                                    mObj["image"].toString(),
+                                    width: 80,
+                                    height: 80,
+                                    fit: BoxFit.cover,
+                                  )),
+                                  const SizedBox(width: 15),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        const SizedBox(height: 12),
+                                        Text(
+                                          mObj["name"].toString(),
+                                          style: TextStyle(
+                                            color: TColor.primaryText,
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 4),
+                                        Text(
+                                          "${mObj["items_count"].toString()} items",
+                                          style: TextStyle(
+                                            color: TColor.secondaryText,
+                                            fontSize: 11,
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
                                 ],
@@ -228,15 +234,14 @@ class _HomeViewState extends State<HomeView> {
                             ),
                             IconButton(
                               onPressed: () {
-                              
                                 /*Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => MenuItemsView(
-                            mObj: mObj,
-                          ),
-                        ),
-                      );*/
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => MenuItemsView(
+                                        mObj: mObj,
+                                      ),
+                                    ),
+                                  );*/
                               },
                               icon: Container(
                                 width: 35,
@@ -259,18 +264,18 @@ class _HomeViewState extends State<HomeView> {
                                   height: 15,
                                 ),
                               ),
-                            )
+                            ),
                           ],
                         ),
-                      ],
-                    ),
-                  );
-                }),
+                      ),
+                    );
+                  }),
+                ),
               ),
             ],
           ),
-        ),
+        ],
       ),
-    ]));
+    );
   }
 }
