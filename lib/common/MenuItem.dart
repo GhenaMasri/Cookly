24d92e40947dff1f5b4725406d87e3158f5e7cd9
GhenaMasri <ghenama77@gmail.com>
2024-05-07 19:08:51 +0300

    Chef home page design

---
+++ lib/common/MenuItem.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+// Define the class
+class MenuItem {
+  int? kitchenId;
+  int? itemId;
+  String? image;
+  String? name;
+  String? notes;
+  String? quantity;
+ 
+  MenuItem({
+    this.kitchenId,
+    this.itemId,
+    this.image,
+    this.name,
+    this.notes,
+    this.quantity,
+  });
+}
