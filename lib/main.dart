fdab9178d7f78d16b260aaf226917c5616d44696
GhenaMasri <ghenama77@gmail.com>
2024-05-13 18:06:59 +0300

    Set splashview as home

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,6 @@ class MyApp extends StatelessWidget {
           fontFamily: "Metropolis",
         ),
         // ignore: prefer_const_constructors
-        home: MainView());
+        home: SplashView());
   }
 }
