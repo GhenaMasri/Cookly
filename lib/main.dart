0c138988ff07bfa56245e887b0a54642b24a50cd
GhenaMasri <ghenama77@gmail.com>
2024-05-20 19:19:39 +0300

    Item Details View

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:untitled/main_page.dart';
 import 'package:untitled/menu/user_kitchens_view.dart';
 import 'package:untitled/more/payment_details_view.dart';
 import 'package:untitled/order/chef_order_view.dart';
+import 'package:untitled/order/item_details_view.dart';
 import 'package:untitled/profile/change_password.dart';
 import 'package:untitled/profile/profile_tab_bar.dart';
 import 'package:untitled/profile/user_profile.dart';
@@ -43,6 +44,6 @@ class MyApp extends StatelessWidget {
           fontFamily: "Metropolis",
         ),
         // ignore: prefer_const_constructors
-        home: SplashView());
+        home: ItemDetailsView());
   }
 }
